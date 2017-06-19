37432d997e4085904fe5390cbef96ef10d03f21f
kougei073 <g1627073@st.t-kougei.ac.jp>
2017-06-19 18:03:49 +0900

    Update default.js

--- default.js
+++ default.js
@@ -1,3 +1,6 @@
+int a;
+a = 3;
+
 var number = Math.floor(Math.random()*3);
 var answer = parseInt(window.prompt('数字当てゲーム　０～２の数字を入力してください'));
 
@@ -8,7 +11,7 @@ message = 'あたり';
 message = 'はずれ　数字はもっと大きいです';
 }else if(number < answer){
 message = 'はずれ　数字はもっと小さいです';
-}else if(i=3; i < answer){
+}else if(a < answer){
 message = '不当な入力が行われました、０～２の数字を入力してください';
 }else{
 message = '不当な入力が行われました、０～２の数字を入力してください';
